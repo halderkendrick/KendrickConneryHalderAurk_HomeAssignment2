4e8d0c23294af457e4e7f96c4a6445e316299e69
Kendrick Halder <108589219+halderkendrick@users.noreply.github.com>
2025-12-07 14:02:06 +0600

    Add files via upload

---
+++ problem_8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+
+    for(int i = num; i >= 1; i--){      
+        for(int j = 1; j <= i; j++){     
+            printf("*");
+        }
+        printf("\n");                   
+    }
+
+    return 0;
+}
